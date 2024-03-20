d8cb5909c899374adb76220487213a10a61ea004
akshay <amg.akshaygaddi@gmail.com>
2024-03-20 11:17:36 +0530

    windows first commit

--- App.tsx
+++ App.tsx
@@ -1,13 +1,23 @@
-import {View, Text} from 'react-native';
-import React from 'react';
-import CollegeGpt from './components/CollegeGpt';
+/* eslint-disable prettier/prettier */
+// import {StyleSheet, Text, View} from 'react-native';
+import * as React from 'react';
+import {Button, View} from 'react-native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
+import CollegeGpt from './screens/CollegeGpt';
+
+const Drawer = createDrawerNavigator();
 
 const App = () => {
   return (
-    <>
-      <CollegeGpt />
-    </>
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="CollegeGpt" component={CollegeGpt} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 };
 
 export default App;
+
+// const styles = StyleSheet.create({});
---
+++ AppStack.tsx
@@ -0,0 +1,15 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import CollegeGpt from './components/CollegeGpt';
+import VertexAI from './components/VertexAI';
+
+const AppStack = () => {
+  return (
+    <>
+      <CollegeGpt />
+      {/* <VertexAI /> */}
+    </>
+  );
+};
+
+export default AppStack;
